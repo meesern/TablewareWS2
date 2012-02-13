173650f96331190765159e65f462649b918738af
Rupert Meese <rupert.meese@nottingham.ac.uk>
2012-02-13 15:47:15 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BusabaDemo::Application.config.secret_token = 'f587845a4071c97337d4cba53b6388d4f77a594ecf2b89aa17800525394738cd653e5b9aceda451e8da3bf65082dbdc472243e5e83501a6ece7320a8a8931b4d'
